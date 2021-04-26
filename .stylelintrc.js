d087d3ce74655237ea77fb56e131fc32a5131a38
qiuzi <2515632823@qq.com>
2021-04-26 18:29:25 +0800

    style: adjust css code

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,9 +2,6 @@ module.exports = {
   root: true,
   extends: ['stylelint-config-qiuzi/scss'],
   rules: {
-    'selector-pseudo-class-no-unknown': [ true, {
-      ignorePseudoClasses: ['export']
-    }],
     'selector-max-class': 6,
     'selector-max-compound-selectors': 6,
     'scss/dollar-variable-pattern': null,
